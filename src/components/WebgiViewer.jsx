06dfcba89b56e0890b0a87aa3755c7a411b5763a
Alaminxab <alaminxab@gmail.com>
2023-05-04 19:16:28 +0100

    webgi canvas

---
+++ src/components/WebgiViewer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WebgiViewer = () => {
+  return (
+    <div id='webgi-canvas-container'>
+        <canvas id=''/>
+    </div>
+  )
+}
+
+export default WebgiViewer
